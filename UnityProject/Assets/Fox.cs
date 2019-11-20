00b1be2a46e8136f50bb59517a80822cd549fef0
cindy01067 <cindy01067@gmail.com>
2019-11-20 21:39:13 +0800

    完成水管隨機

---
+++ UnityProject/Assets/Fox.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class Fox : MonoBehaviour
+{
+    [Header ("Speed") , Range(0 , 100)]
+    public float speed = 0.1F;
+    public Transform Rig;
+    private void Update()
+    {
+      
+    }
+    
+
+}
