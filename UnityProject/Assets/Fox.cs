e8928f5021ebd7c5c03ba2bba0d9e557be590e42
cundy01067 <57181197+cundy01067@users.noreply.github.com>
2019-11-22 21:40:17 +0800

    11/22

--- UnityProject/Assets/Fox.cs
+++ UnityProject/Assets/Fox.cs
@@ -2,13 +2,12 @@
 
 public class Fox : MonoBehaviour
 {
-    [Header ("Speed") , Range(0 , 100)]
+    [Header ("移動速度") , Range(0 , 100)]
     public float speed = 0.1F;
+
     public Transform Rig;
-    private void Update()
-    {
-      
-    }
+
+    public Transform Sr;
     
 
 }
